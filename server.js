275b91dfcb815cd5e3256bd94363434d31027eda
Ahyeong <aylee618@gmail.com>
2021-02-15 16:20:58 +0900

    webpack, babel

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const app = express();
 
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+app.use("/dist", express.static(path.resolve(__dirname, "frontend", "dist")));
 
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const webpack = require('webpack');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    name: "portfolio setting",
+    devtool: 'cheap-module-source-map',
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    entry: {
+        app: ['./frontend/static/js/index'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules\/(?!(axios|@redux-saga|redux-logger))/,
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        ['@babel/preset-env', {
+                            targets: {
+                                browsers: ['> 5% in KR', 'last 2 chrome versions', 'ie > 9']
+                            },
+                            debug: true
+                        }], 
+                    ],
+                    plugins : [
+                        // ['react-refresh/babel'],
+                        ['@babel/plugin-proposal-class-properties'],
+                        ["@babel/plugin-transform-runtime",
+                            {
+                                corejs: 3
+                            }
+                        ]
+                    ]
+                }
+            },
+    ],
+    },
+    plugins: [
+        // new RefreshWebpackPlugin(),
+    ],
+    output: {
+        path: path.join(__dirname, 'frontend', 'dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/frontend/',
+        environment: {
+            arrowFunction: false,
+            const: false,
+            destructuring: false,
+            dynamicImport: false,
+            forOf: false,
+            module: false,
+        }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'frontend'),
+        publicPath: '/frontend/',
+        hot: true,
+        historyApiFallback : {
+            index: '/frontend/index.html'
+        }
+    },
+};
