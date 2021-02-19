ca008e2098975ac968030a1e68efe44da769e66d
Ahyeong <aylee618@gmail.com>
2021-02-19 11:37:24 +0900

    frontend works 추가

--- frontend/static/js/views/frontend/Frontend.js
+++ frontend/static/js/views/frontend/Frontend.js
@@ -85,9 +85,53 @@ export default class extends AbstractView {
                         <img src="./static/images/card-maker.png" alt="Business card maker">
                     </a>
                     <h1 class="project-name">Business Card Maker</h1>
-                    <p class="project-descriptions">Built using React, Realtime Database and Authentication of Firebase, Cloudinary.</p>
+                    <p class="project-descriptions">Built using React JS, HTML, CSS, Firebase(Realtime Database, Authentication) and Cloudinary.</p>
                     <p class="project-descriptions">Deployed using Jenkins, Docker, AWS ECS and AWS CodeDeploy.</p>
                     <a class="code-btn" href="https://github.com/AhyeongLee/business-card-maker" target="_blank">Go to Code</a>
+                </div>                    
+            </article>
+            <article class="frontend-article youtube-clone-article">
+                <div class="project-info">
+                    <a class="image-container" target="_blank" href="https://602f1f4c4b25ab2ffa7fa7e0--trusting-gates-bb8c85.netlify.app/">
+                        <img src="./static/images/youtube-clone.png" alt="Youtube Clone">
+                    </a>
+                    <h1 class="project-name">Youtube Clone</h1>
+                    <p class="project-descriptions">Built using React JS, HTML, CSS, Youtube API.</p>
+                    <p class="project-descriptions">Deployed using Netlify.</p>
+                    <a class="code-btn" href="https://github.com/AhyeongLee/youtube-clone" target="_blank">Go to Code</a>
+                </div>                    
+            </article>
+            <article class="frontend-article">
+                <div class="project-info">
+                    <a class="image-container" target="_blank" href="https://ahyeonglee.github.io/bouncing-ball/index">
+                        <img src="./static/images/bouncing-ball.png" alt="Simple Gallery">
+                    </a>
+                    <h1 class="project-name">Bouncing Ball</h1>
+                    <p class="project-descriptions">Built using Vanilla JS, HTML, CSS</p>
+                    <p class="project-descriptions">Deployed using Github.</p>
+                    <a class="code-btn" href="https://github.com/AhyeongLee/AhyeongLee.github.io/tree/master/bouncing-ball" target="_blank">Go to Code</a>
+                </div>                    
+            </article>
+            <article class="frontend-article">
+                <div class="project-info">
+                    <a class="image-container" target="_blank" href="https://ahyeonglee.github.io/3d-card/index">
+                        <img src="./static/images/3d-card.png" alt="3D Card">
+                    </a>
+                    <h1 class="project-name">3D Card</h1>
+                    <p class="project-descriptions">Built using Vanilla JS, HTML, CSS</p>
+                    <p class="project-descriptions">Deployed using Github.</p>
+                    <a class="code-btn" href="https://github.com/AhyeongLee/AhyeongLee.github.io/tree/master/3d-card" target="_blank">Go to Code</a>
+                </div>                    
+            </article>
+            <article class="frontend-article">
+                <div class="project-info">
+                    <a class="image-container" target="_blank" href="http://simpel-gallery.s3-website.ap-northeast-2.amazonaws.com/">
+                        <img src="./static/images/simple-gallery.png" alt="Simple Gallery">
+                    </a>
+                    <h1 class="project-name">Simple Gallery</h1>
+                    <p class="project-descriptions">Built using Vanilla JS, HTML, CSS</p>
+                    <p class="project-descriptions">Deployed using S3.</p>
+                    <a class="code-btn" href="https://github.com/AhyeongLee/simple-gallery" target="_blank">Go to Code</a>
                 </div>                    
             </article>
         </div>
