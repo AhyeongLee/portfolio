0a065e1f576343ad3168febf5616ddb04693323c
Ahyeong <aylee618@gmail.com>
2021-02-19 11:05:59 +0900

    css 수정

--- frontend/static/js/views/frontend/Frontend.js
+++ frontend/static/js/views/frontend/Frontend.js
@@ -8,86 +8,89 @@ export default class extends AbstractView {
 
     getHtml = async () => {
         return `
-        <h1 class="frontend-h1">My Skills</h1>
-        <div class="project-skills">
-            <div class="circle-box">
-                <div class="skill-container">
-                    <h3 class="skill">React</h3>
-                    <svg class="gauge-container react-container">
-                        <circle class="gauge-border" cx="40" cy="40" r="40"></circle>
-                        <circle class="gauge-bar" cx="40" cy="40" r="40"></circle>
-                    </svg>
+        <article class="skills-article">
+            <h1 class="frontend-h1">My Skills</h1>
+            <div class="project-skills">
+                <div class="circle-box">
+                    <div class="skill-container">
+                        <h3 class="skill">75%<br>JS</h3>
+                        <svg class="gauge-container javascript-gauge-container">
+                            <circle class="gauge-border" cx="50" cy="50" r="50"></circle>
+                            <circle class="gauge-bar" cx="50" cy="50" r="50"></circle>
+                        </svg>
+                    </div>
+                    <div class="skill-container">
+                        <h3 class="skill">75%<br>HTML/CSS</h3>
+                        <svg class="gauge-container html-gauge-container">
+                            <circle class="gauge-border" cx="50" cy="50" r="50"></circle>
+                            <circle class="gauge-bar" cx="50" cy="50" r="50"></circle>
+                        </svg>
+                    </div>
                 </div>
-                <div class="skill-container">
-                    <h3 class="skill">HTML</h3>
-                    <svg class="gauge-container html-container">
-                        <circle class="gauge-border" cx="40" cy="40" r="40"></circle>
-                        <circle class="gauge-bar" cx="40" cy="40" r="40"></circle>
-                    </svg>
+                <div class="circle-box">
+                    <div class="skill-container">
+                        <h3 class="skill">60%<br>React</h3>
+                        <svg class="gauge-container react-gauge-container">
+                            <circle class="gauge-border" cx="50" cy="50" r="50"></circle>
+                            <circle class="gauge-bar" cx="50" cy="50" r="50"></circle>
+                        </svg>
+                    </div>
+                    <div class="skill-container">
+                        <h3 class="skill">30%<br>Webpack</h3>
+                        <svg class="gauge-container webpack-gauge-container">
+                            <circle class="gauge-border" cx="50" cy="50" r="50"></circle>
+                            <circle class="gauge-bar" cx="50" cy="50" r="50"></circle>
+                        </svg>
+                    </div>
+                </div>
+                <div class="circle-box">
+                    <div class="skill-container">
+                        <h3 class="skill">30%<br>Babel</h3>
+                        <svg class="gauge-container babel-gauge-container">
+                            <circle class="gauge-border" cx="50" cy="50" r="50"></circle>
+                            <circle class="gauge-bar" cx="50" cy="50" r="50"></circle>
+                        </svg>
+                    </div>
+                    <div class="skill-container">
+                        <h3 class="skill">80%<br>AWS</h3>
+                        <svg class="gauge-container aws-gauge-container">
+                            <circle class="gauge-border" cx="50" cy="50" r="50"></circle>
+                            <circle class="gauge-bar" cx="50" cy="50" r="50"></circle>
+                        </svg>
+                    </div>
+                </div>
+                <div class="circle-box">
+                    <div class="skill-container">
+                        <h3 class="skill">75%<br>Jenkins</h3>
+                        <svg class="gauge-container jenkins-gauge-container">
+                            <circle class="gauge-border" cx="50" cy="50" r="50"></circle>
+                            <circle class="gauge-bar" cx="50" cy="50" r="50"></circle>
+                        </svg>
+                    </div>
+                    <div class="skill-container">
+                        <h3 class="skill">30%<br>Docker</h3>
+                        <svg class="gauge-container docker-gauge-container">
+                            <circle class="gauge-border" cx="50" cy="50" r="50"></circle>
+                            <circle class="gauge-bar" cx="50" cy="50" r="50"></circle>
+                        </svg>
+                    </div>
                 </div>
             </div>
-            <div class="circle-box">
-                <div class="skill-container">
-                    <h3 class="skill">React</h3>
-                    <svg class="gauge-container react-container">
-                        <circle class="gauge-border" cx="40" cy="40" r="40"></circle>
-                        <circle class="gauge-bar" cx="40" cy="40" r="40"></circle>
-                    </svg>
-                </div>
-                <div class="skill-container">
-                    <h3 class="skill">HTML</h3>
-                    <svg class="gauge-container html-container">
-                        <circle class="gauge-border" cx="40" cy="40" r="40"></circle>
-                        <circle class="gauge-bar" cx="40" cy="40" r="40"></circle>
-                    </svg>
-                </div>
-            </div>
-            <div class="circle-box">
-                <div class="skill-container">
-                    <h3 class="skill">React</h3>
-                    <svg class="gauge-container react-container">
-                        <circle class="gauge-border" cx="40" cy="40" r="40"></circle>
-                        <circle class="gauge-bar" cx="40" cy="40" r="40"></circle>
-                    </svg>
-                </div>
-                <div class="skill-container">
-                    <h3 class="skill">HTML</h3>
-                    <svg class="gauge-container html-container">
-                        <circle class="gauge-border" cx="40" cy="40" r="40"></circle>
-                        <circle class="gauge-bar" cx="40" cy="40" r="40"></circle>
-                    </svg>
-                </div>
-            </div>
-            <div class="circle-box">
-                <div class="skill-container">
-                    <h3 class="skill">CSS</h3>
-                    <svg class="gauge-container css-container">
-                        <circle class="gauge-border" cx="40" cy="40" r="40"></circle>
-                        <circle class="gauge-bar" cx="40" cy="40" r="40"></circle>
-                    </svg>
-                </div>
-                <div class="skill-container">
-                    <h3 class="skill">ETC</h3>
-                    <svg class="gauge-container etc-container">
-                        <circle class="gauge-border" cx="40" cy="40" r="40"></circle>
-                        <circle class="gauge-bar" cx="40" cy="40" r="40"></circle>
-                    </svg>
-                </div>
-            </div>
-        </div>
-        <h1 class="frontend-h1">My Frontend Works</h1>
-        <article class="frontend-article card-maker-article">
-            <div class="project-info">
-                <a class="image-container" target="_blank" href="https://aylee.shop">
-                    <img src="./static/images/card-maker.png" alt="Business card maker">
-                </a>
-                <h1 class="project-name">Business Card Maker</h1>
-                <p class="project-descriptions">Built using React, Realtime Database and Authentication of Firebase, Cloudinary.</p>
-                <p class="project-descriptions">Deployed using Jenkins, Docker, AWS ECS and AWS CodeDeploy.</p>
-                <a class="code-btn" href="https://github.com/AhyeongLee/business-card-maker" target="_blank">Go to Code</a>
-            </div>                    
-            
         </article>
+        <div class="works-container">
+            <h1 class="frontend-h1">My Frontend Works</h1>
+            <article class="frontend-article card-maker-article">
+                <div class="project-info">
+                    <a class="image-container" target="_blank" href="https://aylee.shop">
+                        <img src="./static/images/card-maker.png" alt="Business card maker">
+                    </a>
+                    <h1 class="project-name">Business Card Maker</h1>
+                    <p class="project-descriptions">Built using React, Realtime Database and Authentication of Firebase, Cloudinary.</p>
+                    <p class="project-descriptions">Deployed using Jenkins, Docker, AWS ECS and AWS CodeDeploy.</p>
+                    <a class="code-btn" href="https://github.com/AhyeongLee/business-card-maker" target="_blank">Go to Code</a>
+                </div>                    
+            </article>
+        </div>
         `;
     }
 
